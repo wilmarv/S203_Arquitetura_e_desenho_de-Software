00f57cd5a3794e16c166c735a3acc3f80ad90ddd
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2023-06-19 20:14:51 -0300

    iniciando projeto com start.spring.io

---
+++ src/main/java/br/inatel/labs/labads/padroesarquiteturais/AdsPadroesArquiteturaisApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labads.padroesarquiteturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsPadroesArquiteturaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsPadroesArquiteturaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/labs/labads/padroesarquiteturais/AdsPadroesArquiteturaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labads.padroesarquiteturais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsPadroesArquiteturaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
