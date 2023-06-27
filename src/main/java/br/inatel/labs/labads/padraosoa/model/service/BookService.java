1a1ad4f402bca9ff4c03b3abf6c12f379448b58e
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2023-06-26 21:04:54 -0300

    adiciona uma metodo bookService para simular um BD

--- src/main/java/br/inatel/labs/labads/padraosoa/model/service/BookService.java
+++ src/main/java/br/inatel/labs/labads/padraosoa/model/service/BookService.java
@@ -6,11 +6,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.inatel.labs.labads.padraosoa.model.entity.Book;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class BookService {
 
 	private List<Book> bookList = new ArrayList();
+	
+	@PostConstruct
+	private void setup() {
+		Book b1 = new Book(1L,"Delphin Blanco","Contos");
+		Book b2 = new Book(2L,"Python com Matemática","Programação");
+		Book b3 = new Book(3L,"Cozinha Mineral","Gastronomia");
+		
+		bookList.add(b1);
+		bookList.add(b2);
+		bookList.add(b3);
+		
+	}
 
 	public List<Book> searchBook() {
 		return bookList;
